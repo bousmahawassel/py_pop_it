1035ca4e1e08400fa9987e4e539d64c344ded186
wasselbousmaha <wassel2005@gmail.com>
2021-09-04 00:55:41 +0200

    fixed bugs with intelligent symmetrical algorithm

--- algos2.py
+++ algos2.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def symetric(board, last_move):
     col, rows = last_move
     return 5 - col, rows
@@ -6,10 +9,19 @@ def symetric(board, last_move):
 def intelligent_symetric(board, last_move):
     not_full_cols = []
     for col in range(6):
-        if board[:, col].all():
+        if not board[:, col].all():
             not_full_cols.append(col)
     if len(not_full_cols) > 2:
         return symetric(board, last_move)
+    elif len(not_full_cols) == 1:
+        print(not_full_cols)
+        col = board[:, not_full_cols[0]]
+        print(col)
+        zeros = np.where(col == 0)[0]
+        print(zeros)
+        zeros = np.delete(zeros, 0)
+        print(zeros)
+        return not_full_cols[0], list(zeros)
     else:
         if board[:, not_full_cols[0]].sum() == 5:
             return not_full_cols[1],
